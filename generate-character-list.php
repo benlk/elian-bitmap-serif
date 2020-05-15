840004f2526c3b6f6071221688e026e9b1de5e51
Ben Keith <ben@benlk.com>
2020-05-14 23:59:22 -0400

    Now use lookup table to convert glyph names to characters

--- generate-character-list.php
+++ generate-character-list.php
@@ -5,29 +5,90 @@
  * - that the tile is 9 rows wide
  * - that the files are contained in glyphs/
  */
-$path = './glyphs/';
-$files = scandir( $path );
-$files = array_diff( $files, array( '.', '..' ) );
 
+/**
+ * Turn an array of characters into a string terminated by a newline
+ *
+ * @param Array $row Array of single characters
+ * @echo
+ */
 function output_row( $row ) {
-	echo implode( $row, '' ) . "\n";
+	return implode( $row, '' ) . "\n";
 }
 
-$row = array();
+/**
+ * Draw from the AGLFN to create an array of character name lookups
+ *
+ * @uses fgetcsv
+ *
+ * @return Array of Glyph name => 
+ */
+function generate_lookup_table() {
 
-ob_start();
-
-try {
-	foreach ( $files as $file ) {
-		if ( count( $row ) === 9 ) {
-			output_row( $row );
-			$row = array();
+	$semicolon_delimited = file( './agl-aglfn/aglfn.txt' );
+	foreach ( $semicolon_delimited as $iterator => $row ) {
+		if ( '#' === substr( $row, 0, 1 ) ) {
+			unset( $semicolon_delimited[$iterator] );
 		}
-
-		$row[] = basename( $file, '.png' );
 	}
-} finally {
-	output_row( $row );
+
+	$lookup_table = array();
+
+	foreach ( $semicolon_delimited as $row ) {
+		$array = str_getcsv( $row, ';' );
+		// there are three items in this $array at this time:
+		//  (1) Standard UV or CUS UV--four uppercase hexadecimal digits
+		//  (2) Glyph name--upper/lowercase letters and digits
+		//  (3) Character names: Unicode character names for standard UVs, and
+		//      descriptive names for CUS UVs--uppercase letters, hyphen, and
+		//      space
+		//
+		// Now, we turn that into 2 => 1
+		$lookup_table[$array[1]] = $array[0];
+	}
+
+	return $lookup_table;
 }
 
-echo ob_get_clean();
+/**
+ * Load the files, generate the lists
+ *
+ * @return text of the character list
+ */
+function main() {
+	$path = './glyphs/';
+	$files = scandir( $path );
+	$files = array_diff( $files, array( '.', '..' ) );
+
+	$row = array();
+	$lookup_table = generate_lookup_table();
+
+	ob_start();
+
+	try {
+		foreach ( $files as $file ) {
+			if ( count( $row ) === 9 ) {
+				echo output_row( $row );
+				$row = array();
+			}
+
+			$glyph_name = basename( $file, '.png' );
+			$uv = $lookup_table[$glyph_name];
+
+			// convert hex code to character:
+			// https://stackoverflow.com/a/6058533
+			$character = json_decode(sprintf(
+				'"\u%1$s"',
+				$uv
+			));
+
+			$row[] = $character;
+		}
+	} finally {
+		echo output_row( $row );
+	}
+
+	return ob_get_clean();
+}
+
+echo main();
